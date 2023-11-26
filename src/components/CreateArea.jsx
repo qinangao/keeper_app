b85c00b97e61a57e3468125e310c766b637c16fb
Qinan Gao <qinanagao@gmail.com>
2023-11-26 20:33:47 +0000

    add props

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
   function handleChange(event) {
     const { name, value } = event.target;
-    console.log(value);
     setNote((preNote) => {
       return {
         ...preNote,
@@ -16,17 +15,28 @@ function CreateArea() {
     });
   }
   function handleClick(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
     event.preventDefault();
   }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" onChange={handleChange} />
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
         <textarea
           name="content"
           placeholder="Take a note..."
           rows="3"
           onChange={handleChange}
+          value={note.content}
         />
         <button onClick={handleClick}>Add</button>
       </form>
